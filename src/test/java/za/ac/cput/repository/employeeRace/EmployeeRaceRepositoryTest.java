59388c25cf753c5e9e8fdf0fc952059e64d86b9f
Lihle_N <218193882@mycput.ac.za>
2022-04-10 19:50:37 +0200

    Edited the repository test cases

--- src/test/java/za/ac/cput/repository/employeeRace/EmployeeRaceRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/employeeRace/EmployeeRaceRepositoryTest.java
@@ -1,7 +1,47 @@
 package za.ac.cput.repository.employeeRace;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.EmployeeRace;
+import za.ac.cput.factory.EmployeeRaceFactory;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeRaceRepositoryTest {
+    private static EmployeeRaceRepository repository = EmployeeRaceRepository.getRepository();
+    private static EmployeeRace employeeRace = EmployeeRaceFactory.createEmployeeRace(789, "Black");
 
+    @Test
+    void a_create(){
+        EmployeeRace created = repository.create(employeeRace);
+        assertEquals(created.getRaceId(), employeeRace.getRaceId());
+        System.out.println("Create: "+ created);
+    }
+
+    @Test
+    void b_read(){
+        EmployeeRace read = repository.read(employeeRace.getRaceId());
+        assertNotNull(read);
+        System.out.println("Read: "+ read);
+    }
+
+    @Test
+    void c_update(){
+        EmployeeRace updated = new EmployeeRace.Builder().copy(employeeRace).setRaceName("Indian").build();
+        assertNotNull(repository.update(updated));
+        System.out.println("Update: "+ updated);
+    }
+
+    @Test
+    void d_delete(){
+        boolean success = repository.delete(employeeRace.getRaceId());
+        assertTrue(success);
+        System.out.println("Delete: "+ success);
+    }
+
+    @Test
+    void e_getAll(){
+        System.out.println("Show all: ");
+        System.out.println(repository.getAll());
+    }
 }
+
--- src/test/java/za/ac/cput/repository/order/OrderRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/order/OrderRepositoryTest.java
@@ -1,7 +1,52 @@
 package za.ac.cput.repository.order;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.EmployeeRace;
+import za.ac.cput.entity.Order;
+import za.ac.cput.factory.OrderFactory;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderRepositoryTest {
+    private static OrderRepository repository = OrderRepository.getRepository();
+    private static Order order = OrderFactory.createOrder(789, "pizza", "20,12,2021");
+
+    @Test
+    void a_create(){
+        Order created = repository.create(order);
+        assertEquals(created.getOrderId(), order.getOrderId());
+        System.out.println("Create: "+ created);
+    }
+
+    @Test
+    void b_read(){
+        Order read = repository.read(order.getOrderId());
+        assertNotNull(read);
+        System.out.println("Read: "+ read);
+    }
+
+    @Test
+    void c_update(){
+        Order updated = new Order.Builder().copy(order).setOrderId("Indian").build();
+        assertNotNull(repository.update(updated));
+        System.out.println("Update: "+ updated);
+    }
+
+    @Test
+    void d_delete(){
+        boolean success = repository.delete(order.getOrderId());
+        assertTrue(success);
+        System.out.println("Delete: "+ success);
+    }
+
+    @Test
+    void e_getAll(){
+        System.out.println("Show all: ");
+        System.out.println(repository.getAll());
+    }
+}
+
+
 
 }
