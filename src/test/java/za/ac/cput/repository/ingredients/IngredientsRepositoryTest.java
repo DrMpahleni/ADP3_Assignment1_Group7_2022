dc0884257377157c3db6455a9871ad5ed38020a3
Lihle_N <218193882@mycput.ac.za>
2022-04-10 19:19:57 +0200

    Added Test classes for the repository

---
+++ src/test/java/za/ac/cput/repository/ingredients/IngredientsRepositoryTest.java
@@ -0,0 +1,7 @@
+package za.ac.cput.repository.ingredients;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientsRepositoryTest {
+
+}
---
+++ src/test/java/za/ac/cput/repository/order/OrderRepositoryTest.java
@@ -0,0 +1,7 @@
+package za.ac.cput.repository.order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+}
