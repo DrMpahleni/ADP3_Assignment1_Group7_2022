59270bc65b897c645d1fc69d330090899ffbef2e
vuyisa01 <218192215@mycput.ac.za>
2022-04-10 15:02:59 +0200

    First Commit

---
+++ src/main/java/za/ac/cput/entity/ContactType.java
@@ -0,0 +1,54 @@
+package za.ac.cput.entity;
+
+public class ContactType {
+    private String Id;
+    private String name;
+
+    public ContactType(){
+
+    }
+    private ContactType (Builder builder){
+        this.Id = builder.Id;
+        this.name = builder.name;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactType{" +
+                "ID=" + Id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    public static class Builder{
+        private String Id;
+        private String name;
+
+
+        public Builder setId(String Id) {
+            this.Id = Id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+      public ContactType build(){
+            return new ContactType(this);
+      }
+      public Builder copy(ContactType contactType){
+            this.Id = contactType.Id;
+            this.name = contactType.name;
+            return this;
+      }
+    }
+}
+
---
+++ src/main/java/za/ac/cput/entity/EmployeeGender.java
@@ -0,0 +1,52 @@
+package za.ac.cput.entity;
+
+public class EmployeeGender {
+private String employeeId;
+private String genderId;
+
+public EmployeeGender(){
+
+}
+public EmployeeGender (Builder builder){
+    this.employeeId = builder.employeeId;
+    this.genderId = builder.genderId;
+}
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getGenderId() {
+        return genderId;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeGender{" +
+                "employeeId=" + employeeId +
+                ", genderId=" + genderId +
+                '}';
+    }
+    public static class Builder{
+    private String employeeId, genderId;
+
+        public Builder setEmployeeId(String employeeId) {
+            this.employeeId = employeeId;
+            return this;
+        }
+
+        public Builder setGenderId(String genderId) {
+            this.genderId = genderId;
+            return this;
+        }
+        public EmployeeGender build(){
+            return new EmployeeGender(this);
+        }
+        private Builder copy(EmployeeGender employeeGender){
+            this.employeeId = employeeGender.employeeId;
+            this.genderId = employeeGender.genderId;
+            return this;
+        }
+    }
+
+}
