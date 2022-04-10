5a6bf5c0c8a7c9683d06e8711d57340f82ee2fa3
vuyisa01 <218192215@mycput.ac.za>
2022-04-10 15:03:53 +0200

    Second Commit

---
+++ src/main/java/za/ac/cput/factory/ContactTypeFactory.java
@@ -0,0 +1,14 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.entity.ContactType;
+import za.ac.cput.util.genericHelper;
+
+public class ContactTypeFactory {
+public static ContactType createContactType(String Id, String name){
+    String ID = genericHelper.generateId();
+    ContactType contactType = new ContactType.Builder().setId(Id)
+            .setName(name)
+            .build();
+            return contactType;
+}
+}
