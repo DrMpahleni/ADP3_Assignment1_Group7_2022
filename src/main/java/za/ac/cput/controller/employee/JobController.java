f995a85571102330137b0c9670b4a6afe55f2341
ianlouw <ianlouw1997@gmail.com>
2022-08-06 14:32:14 +0200

    updated classes and added controller classes

---
+++ src/main/java/za/ac/cput/controller/employee/JobController.java
@@ -0,0 +1,58 @@
+package za.ac.cput.controller.employee;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import za.ac.cput.domain.employee.Job;
+import za.ac.cput.service.employee.JobService;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("restaurant/job/")
+@Slf4j
+public class JobController {
+
+    private final JobService jobService;
+
+    @Autowired public JobController (JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<Job> save (@Valid @RequestBody Job job) {
+        log.info("Save request: {}", job);
+        Job save = jobService.save(job);
+        return ResponseEntity.ok(save);
+    }
+
+    private Optional<Job> getById(String jobId){
+        return this.jobService.read(jobId);
+    }
+
+    @GetMapping("read/{jobId}")
+    public ResponseEntity<Job> read (@PathVariable String jobId) {
+        log.info("Read request: {}", jobId);
+        Job job = getById(jobId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(job);
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<Void> delete (@PathVariable String jobId) {
+        log.info("Read request: {}", jobId);
+        this.jobService.deleteById(jobId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Job>> getAll (String jobId) {
+        List<Job> jobs = this.jobService.getAll(jobId);
+        return ResponseEntity.ok(jobs);
+    }
+}
---
+++ src/main/java/za/ac/cput/controller/restaurant/RestaurantController.java
@@ -0,0 +1,58 @@
+package za.ac.cput.controller.restaurant;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import za.ac.cput.domain.restaurant.Restaurant;
+import za.ac.cput.service.restaurant.RestaurantService;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("restaurant/")
+@Slf4j
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @Autowired public RestaurantController (RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<Restaurant> save (@Valid @RequestBody Restaurant restaurant) {
+        log.info("Save request: {}", restaurant);
+        Restaurant save = restaurantService.save(restaurant);
+        return ResponseEntity.ok(save);
+    }
+
+    private Optional<Restaurant> getById(String name) {
+        return this.restaurantService.read(name);
+    }
+
+    @GetMapping("read/{name}")
+    public ResponseEntity<Restaurant> read (@PathVariable String name) {
+        log.info("Read request: {}", name);
+        Restaurant employee = getById(name)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(employee);
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<Void> delete(@PathVariable String name) {
+        log.info("Read request: {}", name);
+        this.restaurantService.deleteById(name);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Restaurant>> getAll(String name) {
+        List<Restaurant> employees = this.restaurantService.getAll(name);
+        return ResponseEntity.ok(employees);
+    }
+}
