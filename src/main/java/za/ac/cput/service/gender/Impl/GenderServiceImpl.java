a7d3e8dbee4624a77e693abab0b9a40ba4c319be
Vuyisa <218192215@mycput.ac.za>
2022-08-12 23:59:21 +0200

    Updated Commits

--- src/main/java/za/ac/cput/service/gender/Impl/GenderServiceImpl.java
+++ src/main/java/za/ac/cput/service/gender/Impl/GenderServiceImpl.java
@@ -6,5 +6,50 @@ package za.ac.cput.service.gender.Impl;
  Due Date: 7 August 2022
   */
 
-public class GenderServiceImpl {
+import za.ac.cput.domain.gender.Gender;
+import za.ac.cput.repository.gender.GenderRepository;
+import za.ac.cput.service.gender.IGenderService;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class GenderServiceImpl implements IGenderService {
+    private static GenderServiceImpl gender =null;
+    private GenderRepository genderRepository;
+
+    public GenderServiceImpl() {
+        this.genderRepository = GenderRepository.getRepository();
+    }
+    public static GenderServiceImpl getGender(){
+
+        if (gender == null) {
+            gender = new GenderServiceImpl();
+        }
+        return gender;
+    }
+
+    @Override
+    public Gender create(Gender gender) {
+        return this.genderRepository.create(gender);
+    }
+
+    @Override
+    public Optional<Gender> read(String employeeId) {
+        return Optional.ofNullable(this.genderRepository.read(employeeId));
+    }
+
+    @Override
+    public Gender update(Gender gender) {
+        return this.genderRepository.update(gender);
+    }
+
+    @Override
+    public void delete(Gender gender) {
+    return;
+    }
+
+    @Override
+    public Set<Gender> getAll() {
+        return this.genderRepository.getAll();
+    }
 }
