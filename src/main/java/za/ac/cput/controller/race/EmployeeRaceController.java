3ac8bfcebbce74fc4ade88e02994def646e8baa3
Lihle_N <218193882@mycput.ac.za>
2022-10-11 20:56:10 +0200

    Updated Race and employee race controller

--- src/main/java/za/ac/cput/controller/race/EmployeeRaceController.java
+++ src/main/java/za/ac/cput/controller/race/EmployeeRaceController.java
@@ -6,45 +6,68 @@ this is EmployeeRace controller class
 Due date: 21 August 2022
  */
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import za.ac.cput.domain.race.EmployeeRace;
-import za.ac.cput.factory.race.EmployeeRaceFactory;
-import za.ac.cput.service.race.impl.EmployeeRaceServiceImpl;
+import za.ac.cput.service.race.IEmployeeRaceService;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @RestController
-@RequestMapping
+@RequestMapping("restuarant/employeeRace")
+@Slf4j
 
 public class EmployeeRaceController {
+    private  IEmployeeRaceService IemployeeRaceService;
+
     @Autowired
-    private EmployeeRaceServiceImpl employeeRaceServiceImpl;
-
-    @PostMapping("/create")
-     public EmployeeRace create(@RequestBody EmployeeRace employeeRace) {
-        EmployeeRace newEmployeeRace = EmployeeRaceFactory.createEmployeeRace(employeeRace.getRaceName());
-
-        return employeeRaceServiceImpl.create(newEmployeeRace);
+    public EmployeeRaceController(IEmployeeRaceService IemployeeRaceService) {
+        this .IemployeeRaceService = IemployeeRaceService;
     }
-    @GetMapping("/read{id}")
-    public Optional<EmployeeRace> read(@PathVariable Integer raceId ) {
-        return employeeRaceServiceImpl.read(raceId);
+    @PostMapping("Save")
+    public ResponseEntity<EmployeeRace> save(@Valid @RequestBody EmployeeRace employeeRace) {
+        log.info("Save request: {}", employeeRace);
+        EmployeeRace save = IemployeeRaceService.create(employeeRace);
+        return ResponseEntity.ok(save);
     }
 
-    @PostMapping("/update")
-    public EmployeeRace update(@RequestBody EmployeeRace employeeRace) {
-        return employeeRaceServiceImpl.update(employeeRace);
+    private Optional<EmployeeRace> getById(Integer raceId) {
+        return this.IemployeeRaceService.read(raceId);
+    }
+
+    @GetMapping("/read{raceId}")
+    public ResponseEntity<EmployeeRace> read(@PathVariable Integer raceId) {
+        log.info("Read request: {} raceId");
+        EmployeeRace employeeRace = getById(raceId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return ResponseEntity.ok(employeeRace);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<EmployeeRace> update(@Valid @RequestBody EmployeeRace employeeRace) {
+        log.info("Update request: {} ", employeeRace);
+        EmployeeRace updated = IemployeeRaceService.update(employeeRace);
+        return ResponseEntity.ok(updated);
     }
     @DeleteMapping("delete/{raceId}")
-    public void delete(@PathVariable String employeeRace){
-        return;
+    public ResponseEntity<Void> delete (@PathVariable Integer raceId){
+        log.info("Delete Request: {} ", raceId);
+        this.IemployeeRaceService.deleteById(raceId);
+        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/getAll")
-    public Set<EmployeeRace> getAll() {
-        return employeeRaceServiceImpl.getAll();
+    @GetMapping("/all")
+    public ResponseEntity<List<EmployeeRace>> getAll(Integer raceId) {
+        List<EmployeeRace> employeeRaces = this.IemployeeRaceService.getAll(raceId);
+        return ResponseEntity.ok(employeeRaces);
     }
 
 }
