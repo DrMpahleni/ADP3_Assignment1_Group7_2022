529bd98515cdc52a7196d1df7ceeef34f3f192d6
Vuyisa <218192215@mycput.ac.za>
2022-10-14 10:37:11 +0200

    JPA Update

--- src/main/java/za/ac/cput/service/gender/Impl/GenderServiceImpl.java
+++ src/main/java/za/ac/cput/service/gender/Impl/GenderServiceImpl.java
@@ -6,49 +6,41 @@ package za.ac.cput.service.gender.Impl;
  Due Date: 7 August 2022
   */
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import za.ac.cput.domain.contact.EmployeeContact;
 import za.ac.cput.domain.gender.Gender;
-import za.ac.cput.repository.gender.GenderRepository;
+import za.ac.cput.repository.gender.IGenderRepository;
 import za.ac.cput.service.gender.IGenderService;
-
 import java.util.Optional;
 import java.util.Set;
+
 @Service
 public class GenderServiceImpl implements IGenderService {
-    private static GenderServiceImpl gender =null;
-    private GenderRepository genderRepository;
+    private IGenderRepository genderRepository;
 
-    public GenderServiceImpl() {
-        this.genderRepository = GenderRepository.getRepository();
-    }
-    public static GenderServiceImpl getGender(){
-
-        if (gender == null) {
-            gender = new GenderServiceImpl();
-        }
-        return gender;
+    public GenderServiceImpl(IGenderRepository genderRepository) {
+        this.genderRepository = genderRepository;
     }
 
+    public GenderServiceImpl(){
+    }
     @Override
     public Gender create(Gender gender) {
-        return this.genderRepository.create(gender);
+        return genderRepository.save(gender);
     }
 
     @Override
     public Optional<Gender> read(String employeeId) {
-        return Optional.ofNullable(this.genderRepository.read(employeeId));
+        return genderRepository.findById(employeeId);
     }
 
     @Override
     public Gender update(Gender gender) {
-        return this.genderRepository.update(gender);
+        return genderRepository.save(gender);
     }
 
     @Override
     public void delete(Gender gender) {
-    return;
+    genderRepository.delete(gender);
     }
 
     @Override
