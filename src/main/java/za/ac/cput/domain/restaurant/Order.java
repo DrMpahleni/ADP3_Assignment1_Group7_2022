6ff818be2684affe1c0ca016bbf450d07f675640
Lihle_N <218193882@mycput.ac.za>
2022-10-26 10:09:51 +0200

    Changed order table name

--- src/main/java/za/ac/cput/domain/restaurant/Order.java
+++ src/main/java/za/ac/cput/domain/restaurant/Order.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "Order")
+@Table(name = "restaurant_Order")
 public class Order implements Serializable {
 
     @Id
